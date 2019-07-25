58df932408d6121a22314cd870405664651e08ac
김성훈 <seongh7800@douzone.com>
2019-07-25 21:35:36 +0900

    수정, 검색 기능 구현

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { list } from 'postcss';
 class App extends React.Component {
   
   state = {
+    isEditMode: -1,
     name: '',
     phone: '',
 
@@ -46,7 +47,8 @@ class App extends React.Component {
    * 등록함수
    */
   _handleCreateContact = () => {
-    const lastId = this.state.contact[this.state.contact.length - 1].id + 1;
+    const length = this.state.contact.length;
+    const lastId = length > 0 ? this.state.contact[this.state.contact.length - 1].id + 1 : 1;
 
     this.setState({
       contact: [
@@ -68,16 +70,33 @@ class App extends React.Component {
   _handleChangeState = (target, value) => {
     this.setState({ [target] : value });
   }
-  // _handleChangeState = event => {
-  //   this.setState({ [event.target.name] : event.target.value });
-  // }
+  
+  /**
+   * 수정모드
+   */
+  _handleToggleEditMode = (id) => {
+    this.setState(prevState => ({ isEditMode: prevState.isEditMode === id ? -1 : id }))
+  }
+
+  /**
+   * 수정값 반영
+   */
+  _handleChangeContact = (index, event) => {
+    const newContact = this.state.contact.slice(0);
+    newContact[index][event.target.name] = event.target.value; 
+    this.setState({contact: newContact});
+  }
   
   /**
    * 뷰
    */
   render(){
 
-    const {contact, name, phone} = this.state;
+    const {contact, name, phone, isEditMode} = this.state;
+    let data = contact;
+    if(name){
+       data = contact.filter(c=> c.name.indexOf(name) !== -1)
+    }
 
     return (
       <div className="App">
@@ -88,21 +107,34 @@ class App extends React.Component {
                 <th>이름</th>
                 <th>연락처</th>
                 <th>-</th>
+                <th>-</th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td><input value={name} name="name" onChange={(event)=>this._handleChangeState('name', event.target.value)} placeholder="name" /></td>
                 <td><input value={phone} name="phone" onChange={(event)=>this._handleChangeState('phone', event.target.value)} placeholder="phone" /></td>
-                <td>
+                <td colSpan="2">
                   <button onClick={()=> this._handleCreateContact()}>New</button>
                 </td>
               </tr>
               {
-                contact.map(item => (
-                  <tr>
-                    <td>{item.name}</td>
-                    <td>{item.phone}</td>
+                data.map((item, index) => (
+                  <tr key={index.toString()}>
+                    {(isEditMode > -1 && isEditMode === item.id) ? (
+                      <>
+                        <td><input name="name" value={item.name} onChange={(event) => this._handleChangeContact(index, event)} /></td>
+                        <td><input name="phone" value={item.phone} onChange={(event) => this._handleChangeContact(index, event)} /></td>
+                      </>
+                    ) : (
+                      <>
+                        <td>{item.name}</td>
+                        <td>{item.phone}</td>
+                      </>
+                    )}
+                    <td>
+                      <button onClick={()=>this._handleToggleEditMode(item.id)}>Edit</button>
+                    </td>
                     <td>
                       <button onClick={()=>this._handleRemoveContact(item.id)}>Delete</button>
                     </td>
